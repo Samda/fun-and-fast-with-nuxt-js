d7d2b6e3bffaa3933494a4e42fc20bc3fd263af3
samda <muy.samda13@gmail.com>
2020-06-11 16:26:46 +0700

    finishes nuxt fundamentals

---
+++ store/posts.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+  all: [
+    {
+      id: "balut",
+      title: "What's Balut?",
+      content: "If someone placed balut on your plate, you might love it .."
+    },
+    {
+      id: "whereisit",
+      title: "Amazing vue",
+      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+      id: "learnvue",
+      title: "Learn Vue and Build Amazing App",
+      content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    }
+  ]
+})
